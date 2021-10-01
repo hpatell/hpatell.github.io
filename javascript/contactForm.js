36766038fc22ed73ec40d8039139ff4fa9cd70bf
Himanshu Patel <hpatell@udel.edu>
2021-09-30 22:56:24 -0400

    phase 2 push

---
+++ javascript/contactForm.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('service_cze33gr', 'template_65d4uoy', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+            window.alert("Message Sent!"); 
+            document.getElementById("contact-form").reset();
+    });
+}
